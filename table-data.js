1049ad9a246cd6397e85d28a7bf7bf05fd1aac19
b <5xbebebebebe@gmail.com>
2013-12-10 14:19:34 -0500

    initial commit

---
+++ table-data.js
@@ -0,0 +1,33 @@
+// $(document).ready(function() {
+
+var tableData = {
+  head: {
+    A: 'Column 1',
+    B: 'Column 2',
+    C: 'Column 3',
+    D: 'Column 4'
+  },
+  body: [
+    {
+      a: 'row 1, col 1',
+      b: 'row 1, col 2',
+      c: 'row 1, col 3',
+      d: 'row 1, col 4'
+    }, {
+      a: 'row 2, col 1',
+      b: 'row 2, col 2',
+      c: 'row 2, col 3',
+      d: 'row 2, col 4'
+    }, {
+      a: 'row 3, col 1',
+      b: 'row 3, col 2',
+      c: 'row 3, col 3',
+      d: 'row 3, col 4'
+    }
+  ]
+};
+
+// $('#table').render(tableData);
+
+
+// });
