c862c4eaf81c253f4337b6a59d3858b69ef89e1f
Be <bebirchall@gmail.com>
2013-12-23 09:49:30 -0500

    transparency list example with directives

--- transparency/list-template.js
+++ transparency/list-template.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
 
+
   $('#list').render(listData);
+
+// below is for list 2, using an array of values to populate the list
+  var directive = {
+    item: {
+      text: function() {
+        return this.value;
+      }
+    }
+  };
+
+$('#list2').render(array, directive);
+
 
 });
